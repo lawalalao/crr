cbcd19772492283cbb50bd3d4c007e63522abe36
LawalAlao <lawalalaoad@gmail.com>
2020-12-06 13:09:07 +0100

    update app  with react router dom and links

--- src/App.js
+++ src/App.js
@@ -2,21 +2,42 @@ import React from "react";
 import './App.css';
 import Authentification from './Authentification';
 import Home from './home'
-
+import Header from './Header'
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link
 } from "react-router-dom";
-
+import ReceivedDeliveries from "./ReceivedDeliveries";
+import Delivery from './Delivery';
 
 
 function App() {
   return (
     <div>
-      <Home />
- 
+      <Router>
+
+        <Switch>
+          <Route path="/home">
+            <Home />
+          </Route>
+
+          <Route path="/ReceivedDeliveries">
+         
+            <ReceivedDeliveries backButton="/home"/>
+          </Route>
+          <Route path="/delivery">
+            
+            <Delivery />
+          </Route>
+          <Route path="/">
+            <Authentification />
+          </Route>
+
+        </Switch>
+
+      </Router>
     </div>
   );
 }
