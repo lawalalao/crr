fd0a25eb7e42d3f8f19d3cecbd989a6f8ccac3ee
LawalAlao <lawalalaoad@gmail.com>
2020-12-06 13:12:29 +0100

    create delivery screen

---
+++ src/Delivery.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Link, useHistory } from "react-router-dom";
+import IconButton from "@material-ui/core/IconButton";
+import HomeIcon from '@material-ui/icons/Home';
+import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
+import { Grid,Paper} from '@material-ui/core'
+
+function Delivery({backButton}) {
+        const history = useHistory();
+        
+        const paperStyle={padding :20,height:'90vh',width:290, margin:"20px auto", backgroundColor:'rgb(225, 225, 250)'}
+    return (
+        <div>
+            
+             
+                <Grid>
+                    <Paper elevation={10} style={paperStyle} >
+                        <Grid align='center' >
+                            <div className="header">
+                            {backButton ? (
+                                <IconButton onClick={() => history.replace(backButton)}>
+                                <ArrowBackIosIcon className="header__icon" fontSize="large" />
+                                </IconButton>
+                                
+                            ) : (
+
+                                <Link to="/ReceivedDeliveries">
+                                <IconButton>
+                                <ArrowBackIosIcon className="header__icon" fontSize="large" />
+                                </IconButton>
+                                </Link>
+                            )}
+                            <Link to="/home">
+                                <IconButton>
+                                    <HomeIcon  className="header__icon" fontSize="large" />
+                                </IconButton>
+                            </Link>
+        
+                        </div>
+                         <h1>Details commandes</h1>
+                        </Grid>   
+                    </Paper>
+                </Grid> 
+            
+               
+        </div>
+    )
+}
+
+export default Delivery
